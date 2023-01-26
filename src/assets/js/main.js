0d2ec3f1a5ead3d9e981c1fb3906445f13821c38
mikiyuito <yuito6001@gmail.com>
2023-01-26 22:37:52 +0900

    vite設定

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,5 @@
+console.log("hello");
 
+const h1 = document.querySelector("h1")
+console.log(h1);
+h1.textContent = "りんご"
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
+import stylelint from 'vite-plugin-stylelint';
 
 export default defineConfig({
-  root: "src",//index.htmlが置かれている場所
+  root: "src",//作業中ディレクトリからindex.htmlが置かれている場所
 
   plugin: [
     legacy({
       targets: ["defaults", "not IE 11"]
+    }),
+    stylelint({
+      fix: true
     })
+
   ],
   build: {
     outDir: "../dist",
+    assetsDir: "assets",
     emptyOutDir: true,
     rollupOptions: {
       //rollupに設定するオプション
