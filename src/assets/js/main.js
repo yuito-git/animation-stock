cfead01abdf8ef1f08399faccc5a51915837dd53
mikiyuito <yuito6001@gmail.com>
2023-01-27 22:30:22 +0900

    依存関係の最適化

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,3 @@
 console.log("hello");
 
 const h1 = document.querySelector("h1")
-console.log(h1);
-h1.textContent = "りんご"
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 import stylelint from 'vite-plugin-stylelint';
+import checker from 'vite-plugin-checker';
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",//作業中ディレクトリからindex.htmlが置かれている場所
@@ -10,17 +12,55 @@ export default defineConfig({
       targets: ["defaults", "not IE 11"]
     }),
     stylelint({
-      fix: true
+      fix: true,
+      build: true
+    }),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.js"',
+      }
     })
 
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
+  server: {
+    port: 3000
+  },
+  optimizeDeps: {
+
+    entries: "src"// Could not auto-determine entry point from rollupOptions or html files and there are no explicit optimizeDeps.include patternsの解決
+  },
+
   build: {
     outDir: "../dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    minify: false,
     rollupOptions: {
       //rollupに設定するオプション
-      input: "src/index.html"//エントリーポイントを変更
+
+      input: "src/index.html",//エントリーポイントを変更
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        format: "es",
+        assetFileNames: ({ name }) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(name ?? '')) {
+            return 'assets/images/[name].[ext]';
+          }
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name].[ext]';
+          }
+          if (/\.js$/.test(name ?? '')) {
+            return 'assets/js/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
 
     },
   }
