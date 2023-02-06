9170ac3c04d1b49e8a0ca143d0327b3a4fa0431a
mikiyuito <yuito6001@gmail.com>
2023-02-06 09:52:59 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // import legacy from "@vitejs/plugin-legacy";//レガシー対応
 import { defineConfig } from "vite";
 import stylelint from 'vite-plugin-stylelint';
-import { resolve } from "path";
+import path from "path";
 import viteImagemin from "vite-plugin-imagemin";
 import globule from "globule";
 import vitePluginPugStatic from '@macropygia/vite-plugin-pug-static'
@@ -20,12 +20,14 @@ const dir = {
 // マルチページの設定
 // ==============================================
 const inputs = {};
-const documents = globule.find([`./${dir.src}/**/*.html`, `./${dir.src}/**/*.pug`], {
-  ignore: [`./${dir.src}/html/**/_*.html`, `./${dir.src}/pug/**/_*.pug`],
+const documents = globule.find([`./${dir.src}/**/*.html`, `./${dir.src}/**/*.pug`, `./${dir.src}/scss/common.scss`], {
+  ignore: [`./${dir.src}/html/**/_*.html`, `./${dir.src}/pug/**/_*.pug`,],
 });
+
 documents.forEach((document) => {
   const fileName = document.replace(`./${dir.src}/`, "");
-  inputs[fileName] = resolve(__dirname, dir.src, fileName);
+  const key = path.parse(document).name
+  inputs[key] = path.resolve(__dirname, dir.src, fileName);
 });
 
 
@@ -47,9 +49,8 @@ export default defineConfig({
     rollupOptions: {
       input: { ...inputs },
       output: {
-        entryFileNames: `assets/js/[name].js`,
-        chunkFileNames: `assets/js/[name].js`,
-        // format: "es",
+        entryFileNames: "assets/js/[name].js",
+        chunkFileNames: "assets/js/[name].js",
         assetFileNames: (assetInfo) => {
           if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(assetInfo.name)) {
             return 'assets/images/[name].[ext]';
