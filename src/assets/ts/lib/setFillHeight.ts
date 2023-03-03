f65e5b4ddb99ee5008a208efd0196d3c6579a4ec
mikiyuito <yuito6001@gmail.com>
2023-03-03 21:31:02 +0900

    コメント追加

--- src/assets/ts/lib/setFillHeight.ts
+++ src/assets/ts/lib/setFillHeight.ts
@@ -1,5 +1,6 @@
-export const setFillHeight = (): string => {
+export const setFillHeight = () => {
   const vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-  return 'hello'
+  return document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
+
+//参考→https://zenn.dev/tak_dcxi/articles/2ac77656aa94c2cd40bf
