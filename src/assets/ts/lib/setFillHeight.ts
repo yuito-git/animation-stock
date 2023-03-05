18cd038f27961c1ad616d1a8062c817caff1c55a
mikiyuito <yuito6001@gmail.com>
2023-03-05 11:13:57 +0900

    単位除去関数追加

--- src/assets/ts/lib/setFillHeight.ts
+++ src/assets/ts/lib/setFillHeight.ts
@@ -1,6 +1,6 @@
 export const setFillHeight = () => {
   const vh = window.innerHeight * 0.01
-  return document.documentElement.style.setProperty('--vh', `${vh}px`)
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
 
-//参考→https://zenn.dev/tak_dcxi/articles/2ac77656aa94c2cd40bf
+// 参考→https://zenn.dev/tak_dcxi/articles/2ac77656aa94c2cd40bf
